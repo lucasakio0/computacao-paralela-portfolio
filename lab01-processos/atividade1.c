dc732ef33819d78e0fdd181f7e6fdb68e271f4ab
lucasakio0 <10425346@mackenzista.com.br>
2026-08-13 17:19:46 +0000

    Atividade 1 resolvida baseado na resolução dada em sala

--- lab01-processos/atividade1.c
+++ lab01-processos/atividade1.c
@@ -3,17 +3,20 @@
 # include <sys/wait.h>
 
 int main () {
- pid_t pid = fork ();
-
- if ( pid < 0) { // Erro
-    fprintf (stderr , " Fork falhou !\n");
-    return 1;
- } else if (pid == 0) { // Processo Filho
-    printf ("Eu sou o filho ! Meu PID e %d, meu pai e %d.\n", getpid () , getppid ());
- } else { // Processo Pai
-    printf ("Eu sou o pai! Meu PID e %d, criei o filho %d.\n", getpid () , pid);
-    wait ( NULL ); // Pai espera o filho terminar
-    printf ("Meu filho terminou , agora eu posso terminar .\n");
+ pid_t id = fork(); //pid_t declara uma variavel chamada ID, e pid_t é um identificador de processos
+ //O fork aqui vai criar um novo processo "duplicando", esse será um filho do processo inicial!
+ if (id<0){
+   fprintf(stderr, "Houve um erro na criação do processo filho!"); //Valor negativo simboliza que houve um erro na criação de processo filho
  }
+ else if (id == 0) {
+   printf("Sou o processo filho!, meu processo e de %d!\n", getpid()); //pid retorna 0 para simbolizar que esse é o filho.
+ }
+ else { //processo pai 
+   printf("Sou o processo pai!, meu processo é de %d!\n", getpid());
+   wait(NULL); // Espera o processo filho terminar
+   printf("O processo filho terminou, então vou finalizar.\n"); 
+ }
+
+
  return 0;
 }
